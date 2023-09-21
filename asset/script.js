92d8eb461bb90431c1db37984062fb6b6deb865b
garototojai <jaysonjjs@gmail.com>
2023-09-20 22:19:23 -0300

    javascript1

--- asset/script.js
+++ asset/script.js
@@ -8,12 +8,12 @@ const resultado = document.querySelector("output")
 const campoTipo = document.querySelector("#tipo")
 const campolayoutSim = document.querySelector("#layout_sim")
 const campolayoutNao = document.querySelector("#layout_nao")
-const campoJs = document.addEventListener("Js")
-const campoprazo = document.querySelector("#prazo")
+const campoJs = document.querySelector("#js")
+const campoPrazo = document.querySelector("#prazo")
 
 campoPrazo.addEventListener("input", function () {
     const labelPrazo = document.querySelector("label[for='prazo']")
-    labelPrazo.innerHTML = `Prazo: ${campoprazo.value} semanas`
+    labelPrazo.innerHTML = `Prazo: ${campoPrazo.value} semanas`
     calcular()
 })
 
@@ -21,7 +21,7 @@ campoQtde.addEventListener("change",calcular)
 campoTipo.addEventListener("change",calcular)
 campolayoutSim.addEventListener("change",calcular)
 campolayoutNao.addEventListener("change",calcular)
-campoJs.addEventListener("chage,calcular")
+campoJs.addEventListener("change",calcular)
 
 
 function calcular(){
@@ -29,16 +29,16 @@ function calcular(){
      console.log("calcular orçamento")
 
      let qtde = campoQtde.value
-     let valor = qtde *100
+     let valor = qtde * 100
 
      if (campoTipo.value == 2) valor += 1000
     
-     if (campolayoutSim.checked == true) valor += qtde * 50
+     if (campolayoutSim.checked ) valor += qtde * 50
      
      if(campoJs.checked ) valor *= 1.1
 
-     let taxaDeUrgencia =1 - campoprazo.value * 0.05
-     valor *= 1 +taxaDeUrgencia
+     let taxaDeUrgencia = 1 - campoPrazo.value * 0.05
+     valor *= 1 + taxaDeUrgencia
 
 
 resultado.innerText = ` R$  ${valor}`
